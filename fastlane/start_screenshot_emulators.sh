0d8ab05d8fae26f59361b05bffcd5e6cc6127077
benkuly <kontakt@folivo.net>
2024-10-10 14:46:59 +0200

    try disable hardware acceleration

--- fastlane/start_screenshot_emulators.sh
+++ fastlane/start_screenshot_emulators.sh
@@ -7,6 +7,6 @@ avdmanager create avd -n Nexus_7 -k "system-images;android-${ANDROID_PLATFORM};g
 avdmanager create avd -n Nexus_10 -k "system-images;android-${ANDROID_PLATFORM};google_apis;arm64-v8a" -d "Nexus 10"
 
 mkdir "emulator_logs"
-emulator -avd Nexus_5 -port 5554 -no-window >> emulator_logs/nexus_5.log 2>&1 &
-emulator -avd Nexus_7 -port 5556 -no-window >> emulator_logs/nexus_7.log 2>&1 &
-emulator -avd Nexus_10 -port 5558 -no-window -prop persist.sys.orientation=landscape >> emulator_logs/nexus_10.log 2>&1 &
+emulator -avd Nexus_5 -port 5554 -no-window -no-accel >> emulator_logs/nexus_5.log 2>&1 &
+emulator -avd Nexus_7 -port 5556 -no-window -no-accel >> emulator_logs/nexus_7.log 2>&1 &
+emulator -avd Nexus_10 -port 5558 -no-window -no-accel -prop persist.sys.orientation=landscape >> emulator_logs/nexus_10.log 2>&1 &

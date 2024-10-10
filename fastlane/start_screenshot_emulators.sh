f4b283f3784532ec4fcf95b2b6ddf5f9931fe4db
benkuly <kontakt@folivo.net>
2024-10-10 20:06:31 +0200

    better debugging

--- fastlane/start_screenshot_emulators.sh
+++ fastlane/start_screenshot_emulators.sh
@@ -9,21 +9,9 @@ avdmanager create avd -n Nexus_10 -k "system-images;android-${ANDROID_PLATFORM};
 
 echo "start emulators"
 mkdir "emulator_logs"
-emulator -avd Nexus_5 -port 5554 -no-window -no-audio -no-accel >> emulator_logs/nexus_5.log 2>&1 &
-emulator -avd Nexus_7 -port 5556 -no-window -no-audio -no-accel >> emulator_logs/nexus_7.log 2>&1 &
-emulator -avd Nexus_10 -port 5558 -no-window -no-audio -no-accel -prop persist.sys.orientation=landscape >> emulator_logs/nexus_10.log 2>&1 &
-
-explain() {
-	if [[ "$1" =~ "not found" ]]; then
-		printf "device not found"
-	elif [[ "$1" =~ "offline" ]]; then
-		printf "device offline"
-	elif [[ "$1" =~ "running" ]]; then
-		printf "booting"
-	else
-		printf "$1"
-	fi
-}
+emulator -avd Nexus_5 -port 5554 -no-window -no-audio -no-boot-anim -no-accel >> emulator_logs/nexus_5.log 2>&1 &
+emulator -avd Nexus_7 -port 5556 -no-window -no-audio -no-boot-anim -no-accel >> emulator_logs/nexus_7.log 2>&1 &
+emulator -avd Nexus_10 -port 5558 -no-window -no-audio -no-boot-anim -no-accel -prop persist.sys.orientation=landscape >> emulator_logs/nexus_10.log 2>&1 &
 
 wait_for_emulator() {
     local adb_port=$1
@@ -38,17 +26,13 @@ wait_for_emulator() {
             echo "Timeout (${timeout} seconds) reached - Failed to start emulator on port ${adb_port}"
             exit 1
         fi
-        out=$(adb -s "emulator-${adb_port}" shell getprop init.svc.bootanim 2>&1 | grep -v '^\*')
-        if [[ "$out" =~ "command not found" ]]; then
-            echo "$out"
-            exit 1
-        fi
-        if [ "$(adb -s "emulator-${adb_port}" shell getprop sys.boot_completed | tr -d '\r')" = "1" ]; then
+        out=$(adb -s "emulator-${adb_port}" shell getprop sys.boot_completed | tr -d '\r')
+        if [ "$out" = "1" ]; then
             break
         fi
         let "r = sec % 5"
         if [[ $r -eq 0 ]]; then
-            echo "Waiting for emulator on port ${adb_port} to start: $(explain "$out")"
+            echo "Waiting for emulator on port ${adb_port} to start (boot_completed=$out))"
         fi
         sleep 1
         let "sec++"

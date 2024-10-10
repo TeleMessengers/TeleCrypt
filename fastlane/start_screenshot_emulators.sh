f54694ece918d8d00018a5c71d1def64334acc0c
benkuly <kontakt@folivo.net>
2024-10-10 12:35:54 +0200

    log emulator output

--- fastlane/start_screenshot_emulators.sh
+++ fastlane/start_screenshot_emulators.sh
@@ -6,6 +6,7 @@ avdmanager create avd -n Nexus_5 -k "system-images;android-${ANDROID_PLATFORM};g
 avdmanager create avd -n Nexus_7 -k "system-images;android-${ANDROID_PLATFORM};google_apis;x86_64" -d "Nexus 7"
 avdmanager create avd -n Nexus_10 -k "system-images;android-${ANDROID_PLATFORM};google_apis;x86_64" -d "Nexus 10"
 
-emulator -avd Nexus_5 -port 5554 -no-window &
-emulator -avd Nexus_7 -port 5556 -no-window &
-emulator -avd Nexus_10 -port 5558 -no-window -prop persist.sys.orientation=landscape &
+mkdir "emulator_logs"
+emulator -avd Nexus_5 -port 5554 -no-window >> emulator_logs/nexus_5.log 2>&1 &
+emulator -avd Nexus_7 -port 5556 -no-window >> emulator_logs/nexus_7.log 2>&1 &
+emulator -avd Nexus_10 -port 5558 -no-window -prop persist.sys.orientation=landscape >> emulator_logs/nexus_10.log 2>&1 &
